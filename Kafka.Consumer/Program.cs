8a73da4706ae7169f9ebd0c938187a506a57246b
Ramnarayan Chinavelaka <ramnarayan.chinavelaka@fmglobal.com>
2019-02-18 22:50:43 -0500

    Initial code

---
+++ Kafka.Consumer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace Kafka.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Kafka endpoint address
+            string kafkaEndpoint = "localhost:9092";
+
+            // The Kafka topic we'll be using
+            string kafkaTopic = "test-topic";
+
+            // Create the consumer configuration
+            var consumerConfig = new Dictionary<string, object>
+            {
+                {"group.id", "my-consumer"},
+                {"bootstrap.servers", kafkaEndpoint},
+                {"enable.auto.commit", "false"}
+            };
+
+            // Create the consumer
+            using (var consumer =
+                new Consumer<Null, string>(consumerConfig, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                // Subscribe to the Kafka topic
+                consumer.Subscribe(new List<string>() { kafkaTopic });
+
+                // Subscribe to the OnMessage event
+                consumer.OnMessage += (obj, msg) =>
+                {
+                    Console.WriteLine(
+                        $"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} Received: {msg.Value}");
+                };
+
+                // Handle Cancel Keypress 
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cancelled = true;
+                };
+
+                Console.WriteLine("Ctrl-C to exit.");
+
+                // Poll for messages
+                while (!cancelled)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace Kafka.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your message. Enter q for quitting");
+            var message = default(string);
+
+            // The Kafka endpoint address
+            string kafkaEndpoint = "localhost:9092";
+
+            // The Kafka topic we'll be using
+            string kafkaTopic = "test-topic";
+
+            while ((message = Console.ReadLine()) != "q")
+            {
+                // Create the producer configuration
+                var producerConfig = new Dictionary<string, object>
+                {
+                    {"bootstrap.servers", kafkaEndpoint}
+                };
+
+                using (var producer = new Producer<Null, string>(producerConfig, null, new StringSerializer(Encoding.UTF8)))
+                {
+                    producer.ProduceAsync(kafkaTopic, null, message).GetAwaiter().GetResult();
+                    producer.Flush(100);
+                }
+            }
+        }
+    }
+}
